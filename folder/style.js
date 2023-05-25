5296b776db7bc68bee1ebaba2292790eda917fd3
Fernanda Fernandes do Nascimento <106761954+Fernandeezz@users.noreply.github.com>
2023-05-25 18:48:42 -0300

    Add files via upload

---
+++ folder/style.js
@@ -0,0 +1,58 @@
+import {
+    Carousel,
+    initTE,
+  } from "tw-elements";
+  initTE({ Carousel });
+
+import {
+    Ripple,
+    initTE,
+  } from "tw-elements";
+  initTE({ Ripple });
+
+import { Select, initTE } from "tw-elements";
+  initTE({ Select });
+
+import {
+    Collapse,
+    Ripple,
+    initTE,
+  } from "tw-elements";
+  initTE({ Collapse, Ripple });
+
+  import {
+    Ripple,
+    Input,
+    initTE,
+  } from "tw-elements";
+  
+  initTE({ Ripple, Input });
+
+  import { Select, initTE } from "tw-elements";
+  initTE({ Select });
+
+  // Initialization for ES Users
+import { Select, initTE } from "tw-elements";
+initTE({ Select });
+
+import {
+  Tab,
+  initTE,
+} from "tw-elements";
+
+initTE({ Tab });
+
+import {
+  Ripple,
+  initTE,
+} from "tw-elements";
+
+initTE({ Ripple });
+
+import {
+  Datepicker,
+  Input,
+  initTE,
+} from "tw-elements";
+
+initTE({ Datepicker, Input });
